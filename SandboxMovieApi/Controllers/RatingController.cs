f5db0e1fe49228ad747feee15760b1206f34fef3
eldio <eldio@DGARCIA-PC>
2024-12-09 21:31:29 -0600

    Add project files.

---
+++ SandboxMovieApi/Controllers/RatingController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SandboxMovieApi.Entities;
+
+namespace SandboxMovieApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RatingController : ControllerBase
+    {
+        [HttpGet("Ratings")]
+        public ActionResult<IEnumerable<Rating>> GetRating()
+        {
+            var rating = new Rating();
+            rating.Id = 1;
+            rating.Name = "Test";
+
+            var ratingList = new List<Rating>();
+            ratingList.Add(rating);
+
+            return Ok(ratingList);
+        }
+
+        /// <summary>
+        /// Retrieves a Rating.
+        /// </summary> 
+        /// <response code="200">Returns the rating object</response>
+        /// <response code="404">If the item can not be found</response>
+        [HttpGet("Ratings/{ratingId}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<Rating> GetRatingById(int ratingId)
+        {
+            var rating = new Rating();
+            rating.Id = 1;
+            rating.Name = "Test";
+
+
+            if (ratingId != rating.Id)
+            {
+                return NotFound("Could not find movie");
+            }
+
+            return Ok(rating);
+        }
+    }
+}
---
+++ SandboxMovieApi/Entities/Rating.cs
@@ -0,0 +1,8 @@
+﻿namespace SandboxMovieApi.Entities
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
